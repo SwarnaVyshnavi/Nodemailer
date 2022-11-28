7b2202d187202ee5bbbae9e39f947f0c679578c9
SwarnaVyshnavi <108234365+SwarnaVyshnavi@users.noreply.github.com>
2022-11-29 01:06:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const nodemailer=require('nodemailer');
+
+const transporter=nodemailer.createTransport( {
+    service:"hotmail",
+    auth:{
+        user:"nodeproject123@outlook.com",
+        pass:"nodeMailer@123"
+    }
+});
+
+const options={
+    from:"nodeproject123@outlook.com",
+    to:"ishaksk199@gmail.com",
+    subject:"sending email with nodejs",
+    text:"its very simple"
+};
+
+transporter.sendMail(options,(err,info)=>{
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log(info);
+})
---
+++ readme.txt
@@ -0,0 +1,11 @@
+Step 1 :Install node in your system.
+
+Step 2 :Install nodemailer module using 'npm i nodemailer' command
+
+Step 4 :In place of user and pass in app.js write sender Email and Password.
+
+step 5 :In options object write the subject and body of Email. Assign receiver mail id in to 'to' attribute in options object.  
+
+step 6 :Run app.js using 'node app' command.
+
+step 7 :Check the inbox or spam for sent mail.
